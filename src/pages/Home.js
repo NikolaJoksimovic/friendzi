5818ee631e8d95cc2d414a6660e10eea3585225f
unknown <nikolajoksimovic95@gmail.com>
2022-11-26 16:27:13 +0100

    add dashboard button function onClick

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,23 +12,30 @@ const Home = () => {
   }
   // *****************************************
 
-  return renderPage();
-};
+  // handles
+  const handleClick = () => {
+    navigate("/dashboard");
+  };
 
-// render page
-const renderPage = function () {
-  return (
-    <div id='home-page' className='height-100 center-flex'>
-      <div className='logo-container center-flex'>
-        <img src={logoImage} alt='logo.png' />
-        <p id='friendzi-title'>FriendZi</p>
+  // render page
+  const renderPage = function () {
+    return (
+      <div id='home-page' className='height-100 center-flex'>
+        <div className='logo-container center-flex'>
+          <img src={logoImage} alt='logo.png' />
+          <p id='friendzi-title'>FriendZi</p>
+        </div>
+        <h1>The weekend is coming and you just wanna have fun!</h1>
+        <div className='btn-container'>
+          <button className='primary-btn' onClick={handleClick}>
+            make new friends
+          </button>
+        </div>
       </div>
-      <h1>The weekend is coming and you just wanna have fun!</h1>
-      <div className='btn-container'>
-        <button className='primary-btn'>make new friends</button>
-      </div>
-    </div>
-  );
+    );
+  };
+
+  return renderPage();
 };
 
 export default Home;
