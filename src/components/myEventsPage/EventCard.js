177ce285c931648982e6f47f638faacadf8586f7
unknown <nikolajoksimovic95@gmail.com>
2022-12-16 14:35:02 +0100

    add events display on myevents page

--- src/components/myEventsPage/EventCard.js
+++ src/components/myEventsPage/EventCard.js
@@ -1,5 +1,19 @@
-const EventCard = () => {
-  return <div>event card</div>;
+const EventCard = ({ eventId, users }) => {
+  const formatedDate = getDateInfo(eventId);
+
+  return (
+    <div className='event-card'>
+      <header>{formatedDate}</header>
+    </div>
+  );
 };
+
+function getDateInfo(eventId) {
+  const day = eventId.substring(0, 2);
+  const month = eventId.substring(2, 4);
+  const time = eventId.substring(4, 6);
+  const activity = eventId.substring(8);
+  return `${day}.${month}. ${time}:00 / ${activity}`;
+}
 
 export default EventCard;
--- src/pages/MyEvents.js
+++ src/pages/MyEvents.js
@@ -3,17 +3,27 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import LoadingPage from "../components/LoadingPage";
 import EventCard from "../components/myEventsPage/EventCard";
+import { useMyCookies } from "../hooks/useMyCookies";
 import urls from "../urls/urls.json";
 
 const MyEvents = () => {
   const navigate = useNavigate();
   const [loading, setLoading] = useState(true);
   const url = urls.url;
+  const [cookies, setCookie, removeCookie] = useMyCookies();
+  const [backendData, setBackendData] = useState([]);
   const getEvents = async () => {
     try {
-      const response = await axios.post(`${url}dashboard/myevents`);
-      setLoading(false);
-    } catch (error) {}
+      const response = await axios.post(`${url}dashboard/myevents`, {
+        user_id: cookies.userId,
+      });
+      if (response) {
+        setBackendData(response.data);
+        setLoading(false);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   useEffect(() => {
@@ -28,7 +38,17 @@ const MyEvents = () => {
         <h1 style={{ textAlign: "center" }}>events</h1>
       </header>
       <div id='event-list-container'>
-        <EventCard></EventCard>
+        {Object.entries(backendData).map((entrie) => {
+          const eventId = entrie[1].event_id;
+          const users = entrie[1].users;
+          return (
+            <EventCard
+              key={eventId}
+              eventId={eventId}
+              users={users}
+            ></EventCard>
+          );
+        })}
       </div>
       <div className='btn-container-secondary'>
         <button
