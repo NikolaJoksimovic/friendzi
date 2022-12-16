6a25e28f44076c881c263af1cde8f255074036c2
unknown <nikolajoksimovic95@gmail.com>
2022-12-16 01:06:12 +0100

    some styling

---
+++ src/components/LoadingPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoadingPage = () => {
+  return (
+    <div style={{ height: "100vh", widh: "100vw", background: "white" }}>
+      <h1>loading...</h1>
+    </div>
+  );
+};
+
+export default LoadingPage;
--- src/pages/MyEvents.js
+++ src/pages/MyEvents.js
@@ -1,5 +1,46 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import LoadingPage from "../components/LoadingPage";
+import EventCard from "../components/myEventsPage/EventCard";
+import urls from "../urls/urls.json";
+
 const MyEvents = () => {
-  return <div>my events</div>;
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+  const url = urls.url;
+  const getEvents = async () => {
+    try {
+      const response = await axios.post(`${url}dashboard/myevents`);
+      setLoading(false);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    getEvents();
+  }, []);
+
+  return loading ? (
+    <LoadingPage></LoadingPage>
+  ) : (
+    <div id='my-events-page' className='height-100 center-flex'>
+      <header>
+        <h1 style={{ textAlign: "center" }}>events</h1>
+      </header>
+      <div id='event-list-container'>
+        <EventCard></EventCard>
+      </div>
+      <div className='btn-container-secondary'>
+        <button
+          type='submit'
+          className='secondary-btn'
+          onClick={() => navigate(-1)}
+        >
+          go back
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default MyEvents;
